ec482859b816047176d14e41a2ea390bedb64654
Johnny OMara <omarajohnj@gmail.com>
2024-02-01 17:21:38 -0600

    join rumble works from entry NAV ONLY

--- src/Components/Entry.tsx
+++ src/Components/Entry.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom'
 import { codeGenerator } from '../Utils';
 import { useQuery, gql, useMutation } from '@apollo/client';
 import { graphql } from 'graphql';
+import { useState } from 'react';
 const RUMBLE_QUERY = gql`
      query {
         rumbles {
@@ -23,7 +24,7 @@ const CREATE_RUMBLE = gql`
 
 export const Entry = () => {
     const navigate = useNavigate()
-
+    const [input, setInput] = useState("")
     const [mutateFunction, {data, loading, error}] = useMutation(CREATE_RUMBLE)
 
     const newRumble = () => {
@@ -32,13 +33,21 @@ export const Entry = () => {
         navigate(`/rumble/${rumbleId}`, {state: {id: rumbleId}})
     }
 
+    const joinRumble = (rumbleId: string) => {
+        navigate(`/rumble/${rumbleId}`, {state: {id: rumbleId}})
+    }
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInput(event.target.value)
+    }
+
 return (
     <div>
         <div>Let's Get Ready to Rumble</div>
         <div>
             <div>Join Rumble</div>
-            <input placeholder='Enter code'/>
-            <button>Go
+            <input placeholder='Enter code' onChange={handleChange} value={input}/>
+            <button onClick={()=>joinRumble(input)}>Go
             </button>
         </div>
         <div>
